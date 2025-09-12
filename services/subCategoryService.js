0df2667ae79ad98bcd584adf68c0e669a24ef86a
sangtran13 <tranngocsang147877@gmail.com>
2025-09-13 00:14:34 +0700

    feat: complete ecommerce API with CRUD, validation, filtering, and pagination

--- services/subCategoryService.js
+++ services/subCategoryService.js
@@ -7,7 +7,7 @@ import ApiFeatures from "../utils/apiFeatures.js";
 import qs from "qs";
 import { createOne, updateOne, deleteOne } from "./handlersFactory.js";
 
-export const setCategoryIdToBody = (req, res, next) => {
+export const setCategoryIdToBody = (req, _res, next) => {
   // If the request is coming from a nested route, set the category from params
   if (!req.body.category && req.params.categoryId) {
     req.body.category = req.params.categoryId;
@@ -17,7 +17,7 @@ export const setCategoryIdToBody = (req, res, next) => {
 
 // Nested route - to get subCategories by category ID
 // GET /api/v1/categories/:categoryId/sub-categories
-export const getSubCategoriesByCategory = (req, res, next) => {
+export const getSubCategoriesByCategory = (req, _res, next) => {
   let filterObject = {};
   if (req.params.categoryId) filterObject = { category: req.params.categoryId };
   req.filterObject = filterObject;
